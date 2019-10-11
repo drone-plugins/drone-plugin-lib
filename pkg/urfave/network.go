60574b42f0572420d7204e584ef9a12327b61f27
Don <don.j.olmstead@gmail.com>
2019-10-11 16:56:15 -0700

    Remove golang 1.13 only value

--- pkg/urfave/network.go
+++ pkg/urfave/network.go
@@ -55,7 +55,6 @@ func NetworkFromContext(ctx *cli.Context) Network {
 			KeepAlive: 30 * time.Second,
 			DualStack: true,
 		}).DialContext,
-		ForceAttemptHTTP2:     true,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
